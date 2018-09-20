35d5a8f7760e20ee042d3222a009565542d3e00d
cryocaustik <cryocaustik@gmail.com>
2018-09-20 01:59:51 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,42 @@
+# mkdocs-semantic
+
+Minimalistic [Semantic-UI](https://semantic-ui.com/) theme for [MkDocs](https://www.mkdocs.org/) with a focus on web content.
+
+
+## Example
+
+[![mkdocs-semantic](img/mkdocs-semantic.png)](img/mkdocs-semantic.png)
+
+<br/>
+
+## Installation & Usage
+
+```python
+# using local terminal
+pip install mkdocs-semantic
+```
+
+```yml
+# in your mkdocs.yml file
+theme: semantic
+```
+<br/>
+
+## Custom Config Variables
+
+- `logo_url`: if specified, will be used in the navbar before the site name
+
+<br/>
+
+## Dependencies
+
+- [Semantic-UI >= 2.4](https://semantic-ui.com/)
+- [highlight.js >= 9.12.0](https://highlightjs.org/)
+- [jQuery >= 3.3.1](https://jquery.com/)
+
+<br/>
+
+## Project Road Map
+
+- [ ] setup demo site 
+- [ ] develop doc-focused variant of theme
---
+++ setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+from os import path
+
+
+VERSION = '0.0.2'
+
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+
+setup(
+    name="mkdocs-semantic",
+    version=VERSION,
+    url='https://github.com/cryocaustik/mkdocs-semantic',
+    license='MIT',
+    description='Semantic-UI Theme for MkDocs',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    author='https://github.com/cryocaustik',
+    author_email='cryocaustik@gmail.com',
+    packages=find_packages(),
+    include_package_data=True,
+    entry_points={
+        'mkdocs.themes': [
+            'semantic = semantic',
+        ]
+    },
+    zip_safe=False
+)
